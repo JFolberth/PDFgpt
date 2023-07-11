cc4290e28b78260db57806cb55b8b73551ec4c79
John Folberth <johnfolberth@gmail.com>
2023-07-11 13:40:58 -0400

    fix key

--- docker/app/app.py
+++ docker/app/app.py
@@ -75,9 +75,8 @@ os.environ['OPENAI_API_TYPE'] = openai.api_type
 default_credential = DefaultAzureCredential()
 
 
-
-endpoint = "https://formrecognizerpocdev.cognitiveservices.azure.com/"
-key = "bca71fc14263496faa8a65b9c841ef51"
+endpoint = client.get_secret('form-recognizer-endpoint').value
+key = client.get_secret('form-recognizer-key').value
 
 
 def process_pdf(data):
